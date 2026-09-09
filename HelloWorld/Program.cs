e4809916a2cd34a3d8e9c54e0a03149e457cff98
Caleb Leopard <leopardcaleb1998@gmail.com>
2026-09-09 10:50:33 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+/*
+* Name: Caleb Leopard
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 8, 2026
+* Description: Hello this is my console
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Caleb Leopard");
+Console.WriteLine("This is my first program at ETSU.");
